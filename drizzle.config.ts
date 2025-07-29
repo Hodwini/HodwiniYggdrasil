1c8c9f5a0ff6007aab7286dae23d0323c90c9d71
nsvk13 <nsvkdev@gmail.com>
2025-07-29 05:38:22 +0300

    chore: update config

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig } from 'drizzle-kit';
+import { defineConfig } from 'drizzle-kit'
 
 export default defineConfig({
   dialect: 'postgresql',
@@ -7,11 +7,11 @@ export default defineConfig({
   dbCredentials: {
     host: process.env.DB_HOST || 'localhost',
     port: Number(process.env.DB_PORT) || 5432,
-    user: process.env.DB_USER || 'postgres', 
+    user: process.env.DB_USERNAME || 'postgres', 
     password: process.env.DB_PASSWORD || '',
-    database: process.env.DB_NAME || 'your_db',
+    database: process.env.DB_NAME || 'typedrasil',
     ssl: false
   },
   verbose: true,
   strict: true,
-});
+})
