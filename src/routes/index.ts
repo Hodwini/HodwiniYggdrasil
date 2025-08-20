2c440059322cf3a46200c2f6f94ed4db3ebbbdbc
nsvk13 <nsvkdev@gmail.com>
2025-08-20 15:17:38 +0300

    hotfix: mount api.router endpoints

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,7 +1,9 @@
 import { Elysia } from "elysia";
 import { authServerRoutes } from "./authserver.route";
 import { sessionServerRoutes } from "./sessionserver.route";
+import { apiRoutes } from "./api.route";
 
 export const routes = new Elysia()
     .use(authServerRoutes)
     .use(sessionServerRoutes)
+    .use(apiRoutes)
